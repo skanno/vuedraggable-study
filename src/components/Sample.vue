0231f2951511988dddcd36dc9fe3114e66d8d56d
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-08-30 18:24:00 +0900

    コンポジションAPIへ書き換え。

--- src/components/Sample.vue
+++ src/components/Sample.vue
@@ -1,40 +1,50 @@
 <template>
-    <div class="hello">
-        <draggable v-model="data" group="people" item-key="id" handle=".handle">
-            <template #item="{element}">
-                <div class="drag-item">
-                    <span class="handle">ハンドル</span>
-                    {{ element.id }}{{ element.content }}
-                </div>
-            </template>
-        </draggable>
-    </div>
+  <div class="hello">
+    <draggable v-model="data.list" item-key="id" handle=".handle">
+      <template #item="{element}">
+        <div class="drag-item">
+          <span class="handle">ハンドル</span>
+          {{ element.id }}{{ element.content }}
+        </div>
+      </template>
+    </draggable>
+  </div>
 </template>
 
 <script>
-import draggable from "vuedraggable";
+import { reactive } from 'vue';
+import draggable from 'vuedraggable';
 
 export default {
-    components: {
-        draggable,
-    },
-    data() {
-        return {
-            data: [
-                {
-                    id: 1,
-                    content: "あいうえお",
-                },
-                {
-                    id: 2,
-                    content: "かきくけこ",
-                },
-                {
-                    id: 3,
-                    content: "さしすせそ",
-                },
-            ],
-        };
-    },
+  components: {
+    draggable,
+  },
+  setup(props) {
+    const data = reactive({
+      list: [
+        {
+          id: 1,
+          content: "あいうえお",
+        },
+        {
+          id: 2,
+          content: "かきくけこ",
+        },
+        {
+          id: 3,
+          content: "さしすせそ",
+        },
+      ],
+    });
+
+    return {data};
+  }
 };
 </script>
+
+<style scoped>
+.drag-item {
+  background: rgb(233, 249, 255);
+  margin: 10px 0;
+}
+</style>
