6fe9d94f61162f3c1aebac4cd6376e3e32623b38
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-09-05 19:10:50 +0900

    ２つのリスト間の移動の処理を実装。

--- src/components/Sample1.vue
+++ src/components/Sample1.vue
@@ -1,9 +1,26 @@
 <template>
-  <div>
-    <draggable v-model="data.list" item-key="no" handle=".handle" @start="drag" @end="drop" @change="change">
+  <div class="row">
+    <draggable class="col-3" group="list" v-model="data.list1" item-key="no" handle=".handle" @start="drag" @end="drop" @change="change($event, '左')">
       <template v-slot:item="{ element }">
         <div class="drag-item">
-          <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+          <div v-bind:class="{card: true, 'text-white': true, 'mb-3': true, 'bg-primary': element.primary, 'bg-secondary': !element.primary}" style="max-width: 18rem;">
+            <div class="card-header handle">
+              <i class='bi bi-arrows-move'></i>
+              No.{{element.no}}
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">{{ element.title }}</h5>
+              <p class="card-text">{{ element.body }}</p>
+            </div>
+          </div>
+        </div>
+      </template>
+    </draggable>
+
+    <draggable class="col-3" group="list" v-model="data.list2" item-key="no" handle=".handle" @start="drag" @end="drop" @change="change($event, '右')">
+      <template v-slot:item="{ element }">
+        <div class="drag-item">
+          <div v-bind:class="{card: true, 'text-white': true, 'mb-3': true,  'bg-primary': element.primary, 'bg-secondary': !element.primary}"  style="max-width: 18rem;">
             <div class="card-header handle">
               <i class='bi bi-arrows-move'></i>
               No.{{element.no}}
@@ -35,72 +52,124 @@ export default {
   },
   setup(props) {
     const data = reactive({
-      list: [
+      list1: [
         {
           no: 1,
           title: 'SUM',
           body: '数値を合計する',
+          primary: true
         },
         {
           no: 2,
           title: 'SUMIF',
           body: '条件を指定して数値を合計する',
+          primary: true
         },
         {
           no: 3,
           title: 'SUMIFS',
           body: '複数の条件を指定して数値を合計する',
+          primary: true
         },
         {
           no: 4,
           title: 'SUBTOTAL',
           body: 'さまざまな集計値を求める',
+          primary: true
         },
         {
           no: 5,
           title: 'AGGREGATE',
           body: 'さまざまな集計値を求める',
+          primary: true
         },
+      ],
+      list2: [
         {
           no: 6,
           title: 'PRODUCT',
           body: '積を求める',
+          primary: false
         },
         {
           no: 7,
           title: 'SUMPRODUCT',
           body: '配列要素の積を合計する',
+          primary: false
         },
         {
           no: 8,
           title: 'SUMSQ',
           body: '平方和を求める',
+          primary: false
         },
         {
           no: 9,
           title: 'SUMX2PY2',
           body: '2つの配列要素の平方和を合計する',
+          primary: false
         },
         {
           no: 10,
           title: 'SUMX2MY2',
           body: '2つの配列要素の平方差を合計する',
+          primary: false
+        },
+      ],
+      list3: [
+        {
+          no: 6,
+          title: 'PRODUCT',
+          body: '積を求める',
+          primary: false
+        },
+        {
+          no: 7,
+          title: 'SUMPRODUCT',
+          body: '配列要素の積を合計する',
+          primary: false
+        },
+        {
+          no: 8,
+          title: 'SUMSQ',
+          body: '平方和を求める',
+          primary: false
+        },
+        {
+          no: 9,
+          title: 'SUMX2PY2',
+          body: '2つの配列要素の平方和を合計する',
+          primary: false
+        },
+        {
+          no: 10,
+          title: 'SUMX2MY2',
+          body: '2つの配列要素の平方差を合計する',
+          primary: false
         },
       ],
     });
 
     const drag = (event) => {
-      console.log('ドラッグ');
+      // console.log('ドラッグ');
     };
 
     const drop = (event) => {
-      console.log('ドロップ');
+      // console.log('ドロップ');
     };
 
-    const change = (event) => {
-      console.log(`${event.moved.oldIndex} -> ${event.moved.newIndex}`);
-      console.log(event.moved.element);
-      toaster.info(`「${event.moved.element.title}」が移動しました。`);
+    const change = (event, type) => {
+      if (event.moved) {
+        toaster.info(`「${event.moved.element.title}」が移動しました。`);
+      }
+
+      if (event.added) {
+        toaster.info(`${type}へ「${event.added.element.title}」を追加しました。`);
+      }
+
+      if (event.removed) {
+        toaster.info(`${type}から「${event.removed.element.title}」を削除しました。`);
+      }
     };
 
     return {
